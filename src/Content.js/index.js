e9397bc6ceef3e64d75c4186d9e83207491cc891
somesetty manideep <104755035+somesettymanideep@users.noreply.github.com>
2022-09-14 02:46:09 -0700

    sucessfully

--- src/Content.js/index.js
+++ src/Content.js/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 import "./index.css"
-import Slider from './Slider.js'
+import Banner from './Banner.js'
 import Images from '../Components/Images.js';
 import Rating from '../Stars/Rating';
 
@@ -19,10 +20,14 @@ const  Content=() =>{
   var settings = {
     dots: true,
     infinite: true,
-    speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay:true
+    autoplay: true,
+    speed: 6000,
+    autoplaySpeed:6000,
+    margin:"10px",
+    cssEase: "linear"
+    
       
   };
   const ratingChanged = (newRating) => {
@@ -93,7 +98,7 @@ const  Content=() =>{
       
       
       <section id="home" class="hero-section" width="100%">
-        <Slider/>
+        <Banner/>
 
         
       
@@ -228,8 +233,8 @@ const  Content=() =>{
       <h5>Rating  </h5>
       </div>
       <div class="single-counter position-relative">
-      <h3 id="secondo2" class="countup">234k </h3>
-      <h5>Happy User</h5>
+      <h3 id="secondo2" class="countup">2.0k+</h3>
+      <h5>Happy Customers</h5>
       </div>
       <div class="single-counter">
       <h3 id="secondo3" class="countup" >244.. </h3>
@@ -254,52 +259,47 @@ const  Content=() =>{
       <section id="gallery" class="gallery-section w-80">
         
       <h2 class="gallery-heading text-center m-5">Gallery</h2>
-        <div className='images-container d-flex mt-5 m-2' >
-       
-            <div className='gallery-image' >
-                <img src="assets/img/gallery/1.jpeg" alt="" width="300px" height="300px" />
-            </div>
-            <div className='gallery-image'>
-                <img src="assets/img/gallery/2.webp" alt="" width="300px" height="300px"/>
-            </div>
-            <div className='gallery-image'>
-                <img src="assets/img/gallery/4.jpg" alt="" width="300px" height="300px"/>
-            </div>
-            <div className='gallery-image'>
-                <img src="assets/img/gallery/1 300x300.jpg" alt="" width="300px" height="300px"/>
-            </div>
-            <div className='gallery-image' >
-                <img src="assets/img/gallery/2 300x300.jpg" alt="" width="300px" height="300px" />
-            </div>
-            <div className='gallery-image'>
-                <img src="assets/img/gallery/3 300x300.jpg" alt="" width="300px" height="300px"/>
-            </div>
-            <div className='gallery-image'>
-                <img src="assets/img/gallery/9 300x300.jpg" alt="" width="300px" height="300px"/>
-            </div>
-            <div className='gallery-image'>
-                <img src="assets/img/gallery/4 300x300.jpg" alt="" width="300px" height="300px"/>
-            </div>
-            <div className='gallery-image' >
-                <img src="assets/img/gallery/6 300x300.jpg" alt="" width="300px" height="300px" />
-            </div>
-            <div className='gallery-image'>
-                <img src="assets/img/gallery/7 300x300.jpg" alt="" width="300px" height="300px"/>
-            </div>
-            <div className='gallery-image'>
-                <img src="assets/img/gallery/8 300x300.jpg" alt="" width="300px" height="300px"/>
-            </div>
-            
-            <div className='gallery-image'>
-                <img src="assets/img/gallery/10 300x300.jpg" alt="" width="300px" height="300px"/>
-            </div>
-            <div className='gallery-image'>
-                <img src="assets/img/gallery/1 300x300.jpg" alt="" width="300px" height="300px"/>
-            </div>
-            <div className='gallery-image'>
-                <img src="assets/img/gallery/12.jpg" alt="" width="300px" height="300px"/>
-            </div>
+        
+      <Slider {...settings}>
+        <div class="gaps d-flex">
+            <img src={ process.env.PUBLIC_URL + "assets/img/gallery/4 600x600.jpg" }  alt="img" />
+           
         </div>
+        <div class="gaps">
+            <img src={ process.env.PUBLIC_URL + "assets/img/gallery/1 600x600.jpg" } alt="img" />
+        </div>
+        <div class="gaps">
+            <img src={ process.env.PUBLIC_URL + "assets/img/gallery/2 600x600.jpg" }  alt="img" />
+        </div>
+        <div class="gaps">
+            <img src={ process.env.PUBLIC_URL + "assets/img/gallery/3 600x600.jpg" } alt="img" />
+        </div>
+        <div class="gaps">
+            <img src={ process.env.PUBLIC_URL + "assets/img/gallery/4 600x600.jpg" } alt="img" />
+        </div>
+        <div class="gaps">
+            <img src={ process.env.PUBLIC_URL + "assets/img/gallery/5 600x600.jpg" } alt="img" />
+        </div>
+        <div class="gaps">
+            <img src={ process.env.PUBLIC_URL + "assets/img/gallery/8 600x600.jpg" } alt="img" />
+        </div>
+        <div class="gaps">
+            <img src={ process.env.PUBLIC_URL + "assets/img/gallery/9 600x600.jpg" } alt="img" />
+        </div>
+        <div class="gaps">
+            <img src={ process.env.PUBLIC_URL + "assets/img/gallery/6 600x600.jpg" } alt="img" />
+        </div>
+        <div>
+            <img src={ process.env.PUBLIC_URL + "assets/img/gallery/7 600x600.jpg" } alt="img" />
+        </div>
+        
+
+
+
+    </Slider>
+       
+           
+      
       </section>
       <section id="how" class="delivery-section pt-150">
       <div class="container">
@@ -310,7 +310,7 @@ const  Content=() =>{
       <span class="wow fadeInUp" data-wow-delay=".2s">Special Offer</span>
       <h1 class="mb-35 wow fadeInUp" data-wow-delay=".4s">Get 20% Discount on Tatto Piercing</h1>
       <p class="mb-35 wow fadeInUp" data-wow-delay=".6s"  style={{textAlign:"justify"}}>Piercings and tattoos are body decorations that go back to ancient times. Body piercing involves making a hole in the skin so that you can insert jewelry. </p>
-      <a href="javscript:void(0)" class="main-btn btn-hover wow fadeInUp" data-wow-delay=".8s">Grab now</a>
+      
       </div>
       </div>
       </div>
@@ -392,13 +392,13 @@ const  Content=() =>{
       <svg href="javascript:void(0)" xmlns="http://www.w3.org/2000/svg" width="14" height="16" fill="white" class="bi bi-telephone" viewBox="0 0 16 16">
   <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
 </svg>
-      <a href="javascript:void(0)" class="ml-2">9347179138</a>
+      <a href="tel:+919347179138" class="ml-2">9347179138</a>
       </li>
       <li >
       <svg xmlns="http://www.w3.org/2000/svg" width="14" height="15" fill="white" class="bi bi-instagram" viewBox="0 0 16 16">
   <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
 </svg>
-      <a href="javascript:void(0)" class="ml-2 align-items-center">Instagram</a>
+      <a href=" https://www.instagram.com/7devils_tattoo/"  target="_blank" class="ml-2 align-items-center">Instagram</a>
       </li>
       
       </ul>
@@ -442,10 +442,16 @@ const  Content=() =>{
       </div>
       
       </div>
+      
+        <a href='https://wa.me/+919347179138'  class="whatsapp" target="_blank"> <img src="assets/img/hero/whatsapp-img.png" width="60px" height="50px" alt="whatsapp"/></a>
+      
       </footer>
+      
       <a href="#home" class="scroll-top btn-hover">
       <i class="lni lni-chevron-up"></i>
       </a>
+      
+    
     </div>
   );
 }
