681113ca2e7931820e6fa055206728f8f07832e2
somesetty manideep <104755035+somesettymanideep@users.noreply.github.com>
2022-09-27 02:50:58 -0700

    logo

--- src/Content.js/index.js
+++ src/Content.js/index.js
@@ -57,7 +57,7 @@ const  Content=() =>{
       <div class="col-lg-12">
       <nav class="navbar navbar-expand-lg">
       <a class="navbar-brand" href="#">
-      <img src="assets/img/logo/final.png" alt="Logo" />
+      <img src="assets/img/logo/devils-tatto.png" alt="Logo" />
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="toggler-icon"></span>
