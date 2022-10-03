717d3b48cf690fda1134612f3128dd9549bb5911
somesetty manideep <104755035+somesettymanideep@users.noreply.github.com>
2022-10-03 02:33:45 -0700

    reviews

--- src/Components/Images.js
+++ src/Components/Images.js
@@ -1,12 +1,12 @@
-{/*import React from "react";
+import React from "react";
 
 import "./images.css";
 const Images=()=>{
     return(
-<div class="review-container  ">
-<iframe  class="reviews " src="https://widget.taggbox.com/107169" width="100%" height="100%" ></iframe>
 
-</div>
+<div class='sk-ww-google-reviews' data-embed-id='95053'></div>
+
+
     )
 }
-export default Images;*/}
+export default Images;
--- src/Content.js/index.js
+++ src/Content.js/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Images from "../Components/Images.js";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
@@ -317,7 +317,7 @@ const  Content=() =>{
                         <h1 class="wow fadeInUp" data-wow-delay=".4s ">What Our Users Says</h1>
                     </div>
                 
-               
+               <Images/>
                     
     </section>
    
